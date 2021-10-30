725acf7e791c8e33676bc2b9dc75e1b8a2771b8c
Saveleva Kristina <savelevakristina1995@yandex.ru>
2021-10-30 03:05:53 +0300

    changes popap

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,26 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__image');
-
-const openPopup = function() {
-    popupElement.classList.add('popup_opened');
-};
-
-const closePopup = function() {
-    popupElement.classList.remove('popup_opened');
-};
-
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-
 let nameProf = document.querySelector('.profile__text');
 let jobProf = document.querySelector('.profile__subtitle');
-
 let formElement = document.querySelector('.popup__forms');
 let nameInput = formElement.querySelector('.popup__name');
 let jobInput = formElement.querySelector('.popup__explane');
 let popupSave = document.querySelector('.popup__form');
 
+//Функция открытия попапа с занесением текущих данных из профиля в value
+const openPopup = function() {
+    popupElement.classList.add('popup_opened');
+    nameInput.value = nameProf.textContent;
+    jobInput.value = jobProf.textContent;
+};
+
+//Функция закрытия попапа
+const closePopup = function() {
+    popupElement.classList.remove('popup_opened');
+};
+
+//функция сохранения данных из попапа в профиль
 function formSubmitHandler (event) {
     event.preventDefault(); 
     jobProf.textContent = jobInput.value;
@@ -28,4 +28,6 @@ function formSubmitHandler (event) {
     closePopup();
 }
 
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 popupSave.addEventListener('submit', formSubmitHandler);
