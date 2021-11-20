fde17a9bb22d8896bda71e5de6e2f34ff2477806
Saveleva Kristina <savelevakristina1995@yandex.ru>
2021-11-20 15:05:27 +0300

    savebutton

--- scripts/validate.js
+++ scripts/validate.js
@@ -14,6 +14,21 @@ function addListenersToForm(form) {
     form.addEventListener('submit', (evt) => {
         evt.preventDefault();
     });
+
+    form.addEventListener('input', handleFormInput);
+    toggleButton(form);
+}
+
+function handleFormInput(evt) {
+    toggleButton(evt.currentTarget);
+}
+
+function toggleButton(form) {
+    const button = form.querySelector('.popup__save');
+    const isFormInvalid = !form.checkValidity();
+
+    button.disabled = isFormInvalid;
+    button.classList.toggle('popup__button_invalid', isFormInvalid);
 }
 
 function addListenersToInput(input) {
@@ -25,10 +40,32 @@ function handleFieldValidation(evt) {
     const errorContainer = document.querySelector(`#${element.id}-error`);
     element.setCustomValidity('');
 
-    element.classList.toggle('form__input_type_error', !element.validity.valid);
-    errorContainer.textContent = element.validationMessage;
+    element.classList.toggle(
+        'form__input_type_error',
+        !element.validity.valid
+    );
 
+    validateLength(element);
+    validateRequired(element);
+    validateURL(element);
+    
+    errorContainer.textContent = element.validationMessage;
+}
+
+function validateLength(element) {
     if (element.validity.tooShort || element.validity.tooLong) {
         element.setCustomValidity('укажите длину от 2 до 150 символов');
+    }
+};
+
+function validateRequired(element) {
+    if (element.validity.valueMissing) {
+        element.setCustomValidity('Поле обязательно');
+    }
+};
+
+function validateURL(element) {
+    if (element.validity.typeMismatch && element.type === 'url') {
+        element.setCustomValidity('Здесь должен быть url');
     }
 }
